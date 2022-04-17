13188b1753158eb185c2e966213ca097e9b89ec3
Leonardo Furone <leonardo.furone@gmail.com>
2022-04-17 19:18:50 -0300

    rollback

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,8 +20,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c0140cc66d10ad0483ad90454108e7ff",
-"/": "c0140cc66d10ad0483ad90454108e7ff",
+"index.html": "a3b5855bf892811fddb82b20088d06c1",
+"/": "a3b5855bf892811fddb82b20088d06c1",
 "main.dart.js": "05ba1b963d9df256e08c1435c49bd23e",
 "manifest.json": "e344d83aa16e4f440b7825a02f4d50bf",
 "version.json": "13cfd46bbbdd4d96cd51b495c574db6c"
